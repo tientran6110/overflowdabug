73b7b504d3cf1996a9b6da592aecc1f6b2841f3d
Personal tientran6110 <tientran6110@gmail.com>
2024-12-23 23:01:42 -0800

    1101122324

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,9 @@
 import QuestionCard from "@/components/cards/QuestionCard";
-import HomeFilters from "@/components/home/HomeFilters";
-import Filter from "@/components/shared/Filter";
+// import HomeFilters from "@/components/home/HomeFilters";
+// import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
 // import Pagination from "@/components/shared/Pagination";
-import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+// import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import {
@@ -58,22 +58,22 @@ export default async function Home({ searchParams }: SearchParamsProps) {
       </div>
 
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-        <LocalSearchbar
+        {/* <LocalSearchbar
           route="/"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
           placeholder="Search for questions"
           otherClasses="flex-1"
-        />
+        /> */}
 
-        <Filter
+        {/* <Filter
           filters={HomePageFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
           containerClasses="hidden max-md:flex"
-        />
+        /> */}
       </div>
 
-      <HomeFilters />
+      {/* <HomeFilters /> */}
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {result.questions.length > 0 ? (
--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import React from "react";
 import Theme from "./Theme";
 import MobileNav from "./MobileNav";
-import GlobalSearch from "../search/GlobalSearch";
+// import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
@@ -21,7 +21,7 @@ const Navbar = () => {
           Overflow da <span className="text-primary-500">Bug</span>
         </p>
       </Link>
-      <GlobalSearch />
+      {/* <GlobalSearch /> */}
       <div className="flex-between gap-5">
         <Theme />
         <SignedIn>
