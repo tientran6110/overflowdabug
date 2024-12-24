c954b2b9c1a2b26f85bedfcb111552cdf3fa4130
Personal tientran6110 <tientran6110@gmail.com>
2024-12-23 23:34:06 -0800

    2333122324

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,13 +1,13 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
-// import { auth } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  // const { userId }: { userId: string | null } = await auth();
+  const { userId }: { userId: string | null } = await auth();
 
-  const userId = "12345";
+  // const userId = "12345";
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
